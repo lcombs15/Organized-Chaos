4d3f73a275ca3b732c9daa43093f2da28441240a
Lucas Combs <l-combs@hotmail.com>
2017-09-27 17:28:11 -0400

    Renaming My Files

--- StudentOrganizer/app/src/main/java/edu/group7/csc415/studentorganizer/LucasHome.java
+++ StudentOrganizer/app/src/main/java/edu/group7/csc415/studentorganizer/LucasHome.java
@@ -3,11 +3,11 @@ package edu.group7.csc415.studentorganizer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Home extends AppCompatActivity {
+public class LucasHome extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_home_lucas);
     }
 }
