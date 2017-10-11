7ad9e039483886af35fed13811e966fe8b2da121
highwai <31321807+highwai@users.noreply.github.com>
2017-10-10 22:08:03 -0400

    Course
    It's weird. Katie helped me to upload it yesterday after class. I thought i uploaded.  not sure what's happened..
    My code hasn't finished yet. it doesn't showed error but just couldn't run the app.

---
+++ CourseMainActivity.java
@@ -0,0 +1,42 @@
+package a17fall.haiweic.com.course;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+public class CourseMainActivity extends FrameLayout
+{
+    private TextView textViewClass1;
+    private TextView textViewClass2;
+    private TextView textViewClass3;
+    private TextView textViewClass4;
+    private TextView textViewClass5;
+
+    public CourseMainActivity(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+        LayoutInflater.from(context).inflate(R.layout.activity_course_main, this);
+        initView();
+    }
+
+    private void initView()
+    {
+        textViewClass1 = (TextView)findViewById(R.id.Course1);
+        textViewClass2 = (TextView)findViewById(R.id.Course2);
+        textViewClass3 = (TextView)findViewById(R.id.Course3);
+        textViewClass4 = (TextView)findViewById(R.id.Course4);
+        textViewClass5 = (TextView)findViewById(R.id.Course5);
+
+    }
+
+    public void setClasses(String[] classes)
+    {
+        textViewClass1.setText(classes[0]);
+        textViewClass2.setText(classes[1]);
+        textViewClass3.setText(classes[2]);
+        textViewClass4.setText(classes[3]);
+        textViewClass5.setText(classes[4]);
+    }
+
+}
