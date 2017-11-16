a70b701f25dbbcef8031b32404dc6e96cc3096cb
DESKTOP-MOOLCNN\haiwe <haiweic89@gmail.com>
2017-11-15 20:32:48 -0500

    Made a comment

--- StudentOrganizer/app/src/main/java/edu/group7/csc415/studentorganizer/settings.java
+++ StudentOrganizer/app/src/main/java/edu/group7/csc415/studentorganizer/settings.java
@@ -15,7 +15,7 @@ public class settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-
+        //comment
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
